0ff5fba027c0a82e3c6702829f13a1865e64fe45
Ian Harrier <github@harrier.us>
2018-06-06 09:42:04 -0500

    Prepare sed command for multiple replacements

--- scripts/app-upgrade.sh
+++ scripts/app-upgrade.sh
@@ -24,7 +24,7 @@ echo "=== Updating environment file. ===========================================
 OLD_JIRA_VERSION=$(grep ^JIRA_VERSION= .env | cut -d = -f 2)
 NEW_JIRA_VERSION=$(grep ^JIRA_VERSION= .env.template | cut -d = -f 2)
 echo "[I] Upgrading JIRA from '$OLD_JIRA_VERSION' to '$NEW_JIRA_VERSION'."
-sed -i.bak "s/^JIRA_VERSION=.*/JIRA_VERSION=$NEW_JIRA_VERSION/g" .env
+sed -i.bak -e "s/^JIRA_VERSION=.*/JIRA_VERSION=$NEW_JIRA_VERSION/g" .env
 
 echo "=== Deleting old images. ======================================================="
 IMAGE_BACKUP=$(docker images ianharrier/jira-backup -q)
